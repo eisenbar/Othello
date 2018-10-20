1b0023bebee2299436ebe54548d02d739e132a1c
Logan Karney <karneyl@mail.gvsu.edu>
2018-10-20 10:14:10 -0400

    Update AI.java
    Added way of adding to possible moves

--- othello/sdks/java/AI/src/com/atomicobject/othello/AI.java
+++ othello/sdks/java/AI/src/com/atomicobject/othello/AI.java
@@ -11,13 +11,54 @@ public class AI {
 		moveList = Arrays.asList(moves).listIterator();
 	}
 
-	public int[] computeMove(GameState state) {
-		System.out.println("AI returning canned move for game state - " + state);
+	 public int[] computeMove(GameState state) {
 
-		//AI
+        playerOne = state.getPlayer() == 1 ? true : false;
 
-		return moveList.next();
-	}
+
+        System.out.println("AI returning canned move for game state - " + state);
+
+        //AI
+        ArrayList<Coordinate> moves = possibleMoves(state.getBoard());
+
+        return moveList.next();
+    }
+
+    public ArrayList<Coordinate> possibleMoves(int[][] board) {
+
+        ArrayList<Coordinate> moves = new ArrayList<Coordinate>();
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 1) {
+                    if (playerOne) {
+
+                        //Checks to see if the spot is open
+                        if (board[i][j] == 0) {
+
+                            //TODO: Add a method that determins if a friendly piece is adjacent
+
+                            //Makes sure spot doesn't try to add an out of bounds piece
+                            if (i != 0)
+                                moves.add(new Coordinate(i - 1, j));
+
+                            if (i != board.length - 1)
+                                moves.add(new Coordinate((i + 1), j));
+
+                            if (j != 0)
+                                moves.add(new Coordinate(i, j - 1));
+
+                            if (j != board[i].length - 1)
+                                moves.add(new Coordinate(i, j + 1));
+
+                        }
+                    }
+                }
+            }
+        }
+
+        return moves;
+    }
 
 }
 
